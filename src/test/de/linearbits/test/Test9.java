3b607a4edc4af52f03a6e83d3d3fa7ad427bcd87
RaffaelBild <raffael.bild@gmx.de>
2017-10-17 15:14:48 +0200

    Print more detailed output

--- src/test/de/linearbits/test/Test9.java
+++ src/test/de/linearbits/test/Test9.java
@@ -19,7 +19,6 @@ package de.linearbits.test;
 import java.io.IOException;
 
 import de.linearbits.suda2.SUDA2;
-import de.linearbits.suda2.SUDA2Statistics;
 import de.linearbits.suda2.Timeable;
 
 /**
@@ -42,26 +41,94 @@ public class Test9 extends AbstractTest{
             "data/test2.csv",   // Whatever
             "data/test3.csv",   // Whatever
             "data/test4.csv",   // FARS
-            "data/test7.csv",   // CUP
             "data/test5.csv",   // IHIS
-            "data/test6.csv"    // SS13ACS
+//            "data/test6.csv",   // SS13ACS
+            "data/test7.csv"    // CUP
         };
         
         for (String file : files) {
+            Timeable.reset();
             int[][] dataset = getData(file);
+            System.out.println("New sets branch");
             System.out.println("Dataset: " + file + " length: " + dataset.length);
             // Process
-            int REPETITIONS = 5;
-            long time = System.currentTimeMillis();
+            int REPETITIONS = dataset.equals("data/test6.csv") ? 1 : 20;
+            long time = System.nanoTime();
             for (int i=0; i<REPETITIONS; i++) {
-                long time2 = System.currentTimeMillis();
-                Timeable.reset();
-                SUDA2Statistics stats = new SUDA2(dataset).getKeyStatistics(0, true);
-                System.out.println(" Run: " + (System.currentTimeMillis() - time2) + " MSUs: " + stats.getNumKeys());
+//                long time2 = System.currentTimeMillis();
+                new SUDA2(dataset).getKeyStatistics(0, true);
+//                System.out.println(" Run: " + (System.currentTimeMillis() - time2));
             }
-            time = (long)((System.currentTimeMillis() - time) / (double)REPETITIONS);
-            System.out.println(" - Average time: " + time);
-            Timeable.printOverview();
+//            time = (long)((System.nanoTime() - time) / (double)REPETITIONS);
+            System.out.println(" - Average time: " + (float)((System.nanoTime() - time) / (double)REPETITIONS / 1e9d));
+            System.out.println("   - Projection time: " + (float)((double)Timeable.methodCallTime[Timeable.METHOD_PROJECTION] / (double)REPETITIONS / 1e9d) + " count: " + (long)((double)Timeable.methodCallCount[Timeable.METHOD_PROJECTION] / (double)REPETITIONS));
+            for (int type : new int[]{Timeable.TYPE_INT_SET_BITS, Timeable.TYPE_INT_SET_HASH, Timeable.TYPE_INT_SET_SMALL}) {
+                System.out.println("");
+                System.out.println("Type " + type + " instances: " + Timeable.instanceCount[type]);
+                System.out.println(" - Intersection time: " + (float)((double)Timeable.typeMethodCallTime[type][Timeable.TYPE_METHOD_INTERSECTION] / (double)REPETITIONS / 1e9d) + " count: " + (long)((double)Timeable.typeMethodCallCount[type][Timeable.TYPE_METHOD_INTERSECTION] / (double)REPETITIONS));
+                printDetails(" - Intersection", type, Timeable.TYPE_METHOD_INTERSECTION, REPETITIONS);
+                System.out.println(" - Special row time: " + (float)((double)Timeable.typeMethodCallTime[type][Timeable.TYPE_METHOD_SPECIALROW] / (double)REPETITIONS / 1e9d) + " count: " + (long)((double)Timeable.typeMethodCallCount[type][Timeable.TYPE_METHOD_SPECIALROW] / (double)REPETITIONS));
+                printDetails(" - Special row", type, Timeable.TYPE_METHOD_SPECIALROW, REPETITIONS);
+                System.out.println(" - Support row time: " + (float)((double)Timeable.typeMethodCallTime[type][Timeable.TYPE_METHOD_SUPPORTROW] / (double)REPETITIONS / 1e9d) + " count: " + (long)((double)Timeable.typeMethodCallCount[type][Timeable.TYPE_METHOD_SUPPORTROW] / (double)REPETITIONS));
+                printDetails(" - Support row", type, Timeable.TYPE_METHOD_SUPPORTROW, REPETITIONS);
+            }
+            System.out.println("");
+            System.out.println("Totals:");
+            printDetailTotals(" - Intersection", Timeable.TYPE_METHOD_INTERSECTION, REPETITIONS);
+            printDetailTotals(" - Special row", Timeable.TYPE_METHOD_SPECIALROW, REPETITIONS);
+            printDetailTotals(" - Support row", Timeable.TYPE_METHOD_SUPPORTROW, REPETITIONS);
         }
+    }
+
+    private static void printDetails(String name, int type, int typeMethod, int reps) {
+        String line = name + " time buckets: ";
+        for (int i=0; i<7; ++i) {
+            line += (float)((double)Timeable.typeMethodSizeTimeBuckets[type][typeMethod][i] / (double)reps / 1e9d) + "    ";
+        }
+        System.out.println(line);
+        
+        line = name + " count buckets: ";
+        for (int i=0; i<7; ++i) {
+            line += (long)((double)Timeable.typeMethodSizeCountBuckets[type][typeMethod][i] / (double)reps) + "    ";
+        }
+        System.out.println(line);
+    }
+    
+    private static void printDetailTotals(String name, int typeMethod, int reps) {
+        String line = name + " times: ";
+        long sum = 0;
+        for (int type : new int[]{Timeable.TYPE_INT_SET_BITS, Timeable.TYPE_INT_SET_HASH, Timeable.TYPE_INT_SET_SMALL}) {
+            sum += Timeable.typeMethodCallTime[type][typeMethod];
+        }
+        line += (float)((double)sum / (double)reps / 1e9d) + "    ";
+        System.out.println(line);
+
+        line = name + " counts: ";
+        sum = 0;
+        for (int type : new int[]{Timeable.TYPE_INT_SET_BITS, Timeable.TYPE_INT_SET_HASH, Timeable.TYPE_INT_SET_SMALL}) {
+            sum += Timeable.typeMethodCallCount[type][typeMethod];
+        }
+        line += (long)((double)sum / (double)reps) + "    ";
+        System.out.println(line);
+
+        line = name + " - time buckets: ";
+        for (int i=0; i<7; ++i) {
+            sum = 0;
+            for (int type : new int[]{Timeable.TYPE_INT_SET_BITS, Timeable.TYPE_INT_SET_HASH, Timeable.TYPE_INT_SET_SMALL}) {
+                sum += Timeable.typeMethodSizeTimeBuckets[type][typeMethod][i];
+            }
+            line += (float)((double)sum / (double)reps / 1e9d) + "    ";
+        }
+        System.out.println(line);
+
+        line = name + " - count buckets: ";
+        for (int i=0; i<7; ++i) {
+            sum = 0;
+            for (int type : new int[]{Timeable.TYPE_INT_SET_BITS, Timeable.TYPE_INT_SET_HASH, Timeable.TYPE_INT_SET_SMALL}) {
+                sum += Timeable.typeMethodSizeCountBuckets[type][typeMethod][i];
+            }
+            line += (long)((double)sum / (double)reps) + "    ";
+        }
+        System.out.println(line);
     }
 }
